57810f309bc8bbf93c2d98cb659628dc95b9fce6
FC-187-ProF <prof@fc-187.ru>
2021-12-16 22:53:08 +0300

    changed to mysql

--- project42/settings.py
+++ project42/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&0agrboj#g*or(vh0p^0(!a+z0i*zg$@=9kstiz#a+b=0muk-u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'project42.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_db',
         'USER' : 'project42',
         'PASSWORD' : 'Password',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
--- update catalog.cmd
+++ update catalog.cmd
@@ -1,2 +1,2 @@
 python manage.py makemigrations
-python manage.py migrate catalog
+python manage.py migrate
